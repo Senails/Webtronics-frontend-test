e31d42533154b716d8730818a5961d86da10021b
Senails <76501177+Senails@users.noreply.github.com>
2023-01-20 19:18:19 +0300

    rename atribute

--- shared/components/Input/index.tsx
+++ shared/components/Input/index.tsx
@@ -14,22 +14,22 @@ export function Input({placholder,error,disabled,value,onChange}:props){
         {//svg
             error?
             <svg width="493" height="70" viewBox="0 0 493 70" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <rect x="0.5" y="0.5" width="492" height="69" rx="7.5" fill="#FFA5A5" fill-opacity="0.2"/>
+            <rect x="0.5" y="0.5" width="492" height="69" rx="7.5" fill="#FFA5A5" fillOpacity="0.2"/>
             <rect x="0.5" y="0.5" width="492" height="69" rx="7.5" stroke="#FD4646"/>
             </svg>
             :disabled?
             <svg width="493" height="70" viewBox="0 0 493 70" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <rect x="0.5" y="0.5" width="492" height="69" rx="7.5" fill="white" fill-opacity="0.05"/>
+            <rect x="0.5" y="0.5" width="492" height="69" rx="7.5" fill="white" fillOpacity="0.05"/>
             <rect x="0.5" y="0.5" width="492" height="69" rx="7.5" stroke="#636363"/>
             </svg>
             :
             <svg width="493" height="70" viewBox="0 0 493 70" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <rect width="493" height="70" rx="8" fill="white" fill-opacity="0.05"/>
-            <rect x="0.5" y="0.5" width="492" height="69" rx="7.5" stroke="url(#paint0_linear_2043_234)" stroke-opacity="0.7"/>
+            <rect width="493" height="70" rx="8" fill="white" fillOpacity="0.05"/>
+            <rect x="0.5" y="0.5" width="492" height="69" rx="7.5" stroke="url(#paint0_linear_2043_234)" strokeOpacity="0.7"/>
             <defs>
             <linearGradient id="paint0_linear_2043_234" x1="54.23" y1="6.58924" x2="61.4203" y2="102.392" gradientUnits="userSpaceOnUse">
-            <stop stop-color="#15BFFD"/>
-            <stop offset="1" stop-color="#9C37FD"/>
+            <stop stopColor="#15BFFD"/>
+            <stop offset="1" stopColor="#9C37FD"/>
             </linearGradient>
             </defs>
             </svg>
